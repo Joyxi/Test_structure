ea99928cb915e7a3378753ffba4c4fd60da0dc1e
emma <>
2020-01-23 10:34:25 +0100

    Update test_struct.c

--- test_struct.c
+++ test_struct.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+struct Produit{
+	float prix;
+	float cout;
+	float tva;
+};
+
+
 int main (){
+	struct Produit iphone;
+	printf("Quel est le prix de votre produit ?\n");
+	scanf("%d",&iphone.prix);
 	
 	
 }
